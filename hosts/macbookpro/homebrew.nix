73b11336c6ddb258758b06fd80ef95c9a344593b
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-07-28 14:34:47 +0530

    Add claude homebrew package

--- hosts/macbookpro/homebrew.nix
+++ hosts/macbookpro/homebrew.nix
@@ -1,52 +1,50 @@
 {
-	enable = true;
-	onActivation.autoUpdate = true;
-	onActivation.cleanup = "uninstall";
-	onActivation.upgrade = true;
+  enable = true;
+  onActivation.autoUpdate = true;
+  onActivation.cleanup = "uninstall";
+  onActivation.upgrade = true;
 
-	taps = [];
+  taps = [ ];
 
-	brews = [
-		"gh"
-		"mas"
-	];
+  brews = [ "gh" "mas" ];
 
-	casks = [
-		"adobe-acrobat-reader"
-		"affinity-designer"
-		"affinity-photo"
-		"affinity-publisher"
-		"alcove"
-		"bitwarden"
-		"cleanshot"
-		"clop"
-		"ghostty"
-		"latest"
-		"ledger-live"
-		"logi-options+"
-		"microsoft-office"
-		"microsoft-teams"
-		"obsidian"
-		"omnissa-horizon-client"
-		"pdf-expert"
-		"raspberry-pi-imager"
-		"the-unarchiver"
-		"transmission"
-		"unclack"
-		"visual-studio-code"
-		"whatsapp"
-		"zoom"
-		"zotero"
-	];
+  casks = [
+    "adobe-acrobat-reader"
+    "affinity-designer"
+    "affinity-photo"
+    "affinity-publisher"
+    "alcove"
+    "bitwarden"
+    "claude"
+    "cleanshot"
+    "clop"
+    "ghostty"
+    "latest"
+    "ledger-live"
+    "logi-options+"
+    "microsoft-office"
+    "microsoft-teams"
+    "obsidian"
+    "omnissa-horizon-client"
+    "pdf-expert"
+    "raspberry-pi-imager"
+    "the-unarchiver"
+    "transmission"
+    "unclack"
+    "visual-studio-code"
+    "whatsapp"
+    "zoom"
+    "zotero"
+  ];
 
-	masApps = {
-		Amphetamine = 937984704;
-		Dropover = 1355679052;
-		HandMirror = 1502839586;
-		Meeter = 1510445899;
-		Noir = 1592917505;
-		Reeder = 6475002485;
-		Things3 = 904280696;
-		WireGuard = 1451685025;
-	};
+  masApps = {
+    Amphetamine = 937984704;
+    Dropover = 1355679052;
+    HandMirror = 1502839586;
+    Meeter = 1510445899;
+    Noir = 1592917505;
+    Reeder = 6475002485;
+    Things3 = 904280696;
+    WireGuard = 1451685025;
+  };
 }
