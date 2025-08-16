25706bd7e7178e47331383a587892bfc01a70d2b
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-08-16 15:29:42 +0200

    Set user password using sops-nix

--- users/alex/default.nix
+++ users/alex/default.nix
@@ -1,56 +1,56 @@
-{ config, pkgs, ... }:
+{ config, host, lib, pkgs, ... }:
 
 let
 
   python3-with-packages = pkgs.python3.withPackages (
-	ps: with ps; [
-	  ipykernel
-	  jupyter
-	  matplotlib
-	  numpy
-	  pandas
-	  polars
-	  pyarrow
-	  scikit-learn
-	  scipy
-	  seaborn
-	  statsmodels
-	]
+    ps: with ps; [
+      ipykernel
+      jupyter
+      matplotlib
+      numpy
+      pandas
+      polars
+      pyarrow
+      scikit-learn
+      scipy
+      seaborn
+      statsmodels
+    ]
   );
 
   r-packages = with pkgs.rPackages; [
-	brms
-	car
-	cardx
-	cli
-	DBI
-	effects
-	emmeans
-	ggeffects
-	ggsurvfit
-	ggpubr
-	gamm4
-	glmmTMB
-	gtsummary
-	Hmisc
-	IRkernel
-	janitor
-	knitr
-	labelled
-	languageserver
-	lme4
-	mgcv
-	nls_multstart
-	nlme
-	psych
-	readxl
-	renv
-	robustlmm
-	rmarkdown
-	RSQLite
-	sjPlot
-	tidyverse
-	writexl
+    brms
+    car
+    cardx
+    cli
+    DBI
+    effects
+    emmeans
+    ggeffects
+    ggsurvfit
+    ggpubr
+    gamm4
+    glmmTMB
+    gtsummary
+    Hmisc
+    IRkernel
+    janitor
+    knitr
+    labelled
+    languageserver
+    lme4
+    mgcv
+    nls_multstart
+    nlme
+    psych
+    readxl
+    renv
+    robustlmm
+    rmarkdown
+    RSQLite
+    sjPlot
+    tidyverse
+    writexl
   ];
 
   r-with-packages = pkgs.rWrapper.override { packages = r-packages; };
@@ -71,8 +71,27 @@ in
 
   programs = import ./programs { inherit pkgs; };
 
+  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+  sops.age.generateKey = true;
+
+  sops.secrets."github/token" = {
+    sopsFile = ./secrets/${host}.yml;
+    format = "yaml";
+    key = "github/token";
+  };
+
+  sops.templates."gh-hosts.yml".content = ''
+    github.com:
+      user: acpuchades
+      git_protocol: https
+      oauth_token: ${config.sops.placeholder."github/token"}
+  '';
+
   # link the configuration file in current directory to the specified location in home directory
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+  home.file.".config/gh/hosts.yml".source =
+    config.lib.file.mkOutOfStoreSymlink
+      config.sops.templates."gh-hosts.yml".path;
 
   # link all files in `./scripts` to `~/.config/i3/scripts`
   # home.file.".config/i3/scripts" = {
@@ -80,16 +99,13 @@ in
   #   recursive = true;   # link recursively
   #   executable = true;  # make all files executable
   # };
-  home.file.".config/gh/hosts.yml".source =
-	config.sops.templates."alex/gh-hosts.yml".path;
-
   home.file.".emacs.d" = {
-	source = ./files/emacs.d;
-	recursive = true;
+    source = ./files/emacs.d;
+    recursive = true;
   };
 
   home.file.".config/starship.toml" = {
-	source = ./files/starship.toml;
+    source = ./files/starship.toml;
   };
 
   # encode the file content in nix configuration file directly
@@ -108,74 +124,76 @@ in
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
 
-	# Fonts
-	emacs-all-the-icons-fonts
-	font-awesome
-	nerd-fonts.fira-code
+    # Fonts
+    emacs-all-the-icons-fonts
+    font-awesome
+    nerd-fonts.fira-code
 
-	# System
-	bat
-	direnv
-	delta
-	eza
-	fd
-	fastfetch
-	jq
-	ripgrep
-	vim
+    # System
+    bat
+    direnv
+    delta
+    eza
+    fd
+    fastfetch
+    jq
+    ripgrep
+    vim
 
-	# Network
-	curl
-	nmap
-	netcat
-	samba
-	wget
+    # Network
+    curl
+    nmap
+    netcat
+    samba
+    wget
 
-	# Encryption
-	age
-	gnupg
-	mkpasswd
+    # Encryption
+    age
+    gnupg
+    mkpasswd
 
-	# Dev
-	awscli2
-	docker
+    # Dev
+    awscli2
+    docker
 
-	# Nix
-	nil
-	nixd
-	nixfmt-rfc-style
-	sops
-	ssh-to-age
+    # Nix
+    nil
+    nixd
+    nixfmt-rfc-style
+    sops
+    ssh-to-age
 
-	# Python
-	black
-	pyright
-	python3-with-packages
-	pyenv
-	poetry
-	ruff
-	uv
-	virtualenv
+    # Python
+    black
+    pyright
+    python3-with-packages
+    pyenv
+    poetry
+    ruff
+    uv
+    virtualenv
 
-	# R
-	pandoc
-	texliveSmall
-	r-with-packages
-	radian-with-packages
-	rstudio-with-packages
+    # R
+    pandoc
+    texliveSmall
+    r-with-packages
+    radian-with-packages
+    rstudio-with-packages
   ];
 
   home.sessionVariables = {
-	EDITOR = "emacs";
-	LANG = "es_ES.UTF-8";
-	LC_ALL = "es_ES.UTF-8";
-	PAGER = "bat --paging=always";
+    EDITOR = "emacs";
+    LANG = "es_ES.UTF-8";
+    LC_ALL = "es_ES.UTF-8";
+    LC_TIME = "en_DK.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    PAGER = "bat --paging=always";
   };
 
   home.shellAliases = {
-	l = "ls";
-	la = "ls -A";
-	ll = "ls -lh";
-	lla = "la -lhA";
+    l = "ls";
+    la = "ls -A";
+    ll = "ls -lh";
+    lla = "la -lhA";
   };
 }
