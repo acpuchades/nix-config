9db3ac5d0cb80b770fa736d3075ed68710977c8d
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-08-16 22:55:24 +0200

    Add iphone public key to authorized keys

--- hosts/homeserver/users.nix
+++ hosts/homeserver/users.nix
@@ -20,6 +20,7 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINToP7vyGXG7vrxR8W3T3I2NalZkc1IPd0WaETssf1X5 alex@macbookpro"
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGInkVDbDTNXoUs27tUFuU1sFRRKzZQh1hc0aFyW6xMtyK9olt0lgFGvi1TauS4twSPf4a5UeDLtrkNxSahIAqc= alex@iphone"
     ];
   };
 }
