52e7bd6d3cf0f2a1563dcf15ea41215f2fd5d550
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-08-16 11:14:03 +0200

    Enable gh cli

--- users/alex/programs/default.nix
+++ users/alex/programs/default.nix
@@ -4,6 +4,7 @@ inputs@{ pkgs }:
   home-manager.enable = true;
 
   emacs = import ./emacs.nix inputs;
+  gh = import ./gh.nix inputs;
   git = import ./git.nix inputs;
   starship = import ./starship.nix inputs;
   zed-editor = import ./zed-editor.nix inputs;
