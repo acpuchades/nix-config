62eee25bfa05ff57403e0b75ae7f4a792d36b46d
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-08-17 15:20:12 +0200

    Customize multiple-cursor bindings in emacs

--- users/alex/files/emacs.d/init.el
+++ users/alex/files/emacs.d/init.el
@@ -234,8 +234,8 @@
 (use-package multiple-cursors
   :bind
   (("C-S-c C-S-c" . mc/edit-lines)
-   ("C->"         . mc/mark-next-like-this)
-   ("C-<"         . mc/mark-previous-like-this)
+   ("C-S-<down>"  . mc/mark-next-like-this)
+   ("C-S-<up>"    . mc/mark-previous-like-this)
    ("C-c C-<"     . mc/mark-all-like-this)))
 
 ;; Nix
