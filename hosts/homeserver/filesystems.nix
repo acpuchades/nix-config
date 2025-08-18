6dbf6630584850c703fc1c26607aab95eec0af43
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-08-18 09:07:03 +0200

    Add secondary disk for service storage

--- hosts/homeserver/filesystems.nix
+++ hosts/homeserver/filesystems.nix
@@ -1,13 +1,20 @@
 { ... }:
 {
   "/" = {
-	device = "/dev/disk/by-uuid/851be909-0cca-43a0-83c2-a36e353c28a5";
-	fsType = "ext4";
-	options = [ "noatime" "nodiratime" ];
+    device = "/dev/disk/by-uuid/851be909-0cca-43a0-83c2-a36e353c28a5";
+    fsType = "ext4";
+    options = [ "noatime" ];
   };
 
   "/boot" = {
-	device = "/dev/disk/by-uuid/AA54-7CAB";
-	fsType = "vfat";
+    device = "/dev/disk/by-uuid/AA54-7CAB";
+    fsType = "vfat";
   };
+
+  "/srv" = {
+    device = "/dev/disk/by-uuid/4f9f29bd-6a19-4cfa-8163-9be6ee7171f5";
+    fsType = "btrfs";
+    options = [ "noatime" ];
+  };
+
 }
