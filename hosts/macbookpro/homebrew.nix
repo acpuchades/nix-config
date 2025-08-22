33d12c39a053dfbe7c766b91154465f288932ab8
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-08-22 22:26:54 +0200

    Use spaces instead of tabs for indentation in nix files

--- hosts/macbookpro/homebrew.nix
+++ hosts/macbookpro/homebrew.nix
@@ -8,48 +8,48 @@
   taps = [ ];
 
   brews = [
-	"mas"
+  "mas"
   ];
 
   casks = [
-	"adobe-acrobat-reader"
-	"affinity-designer"
-	"affinity-photo"
-	"affinity-publisher"
-	"alcove"
-	"balenaetcher"
-	"bitwarden"
-	"blender"
-	"claude"
-	"cleanshot"
-	"clop"
-	"ghostty"
-	"ledger-live"
-	"logi-options+"
-	"macfuse"
-	"microsoft-office"
-	"microsoft-teams"
-	"obsidian"
-	"omnissa-horizon-client"
-	"pdf-expert"
-	"telegram"
-	"the-unarchiver"
-	"transmission"
-	"unclack"
-	"whatsapp"
-	"zoom"
-	"zotero"
+    "adobe-acrobat-reader"
+    "affinity-designer"
+    "affinity-photo"
+    "affinity-publisher"
+    "alcove"
+    "balenaetcher"
+    "bitwarden"
+    "blender"
+    "claude"
+    "cleanshot"
+    "clop"
+    "ghostty"
+    "ledger-live"
+    "logi-options+"
+    "macfuse"
+    "microsoft-office"
+    "microsoft-teams"
+    "obsidian"
+    "omnissa-horizon-client"
+    "pdf-expert"
+    "telegram"
+    "the-unarchiver"
+    "transmission"
+    "unclack"
+    "whatsapp"
+    "zoom"
+    "zotero"
   ];
 
   masApps = {
-	Amphetamine = 937984704;
-	Dropover = 1355679052;
-	HandMirror = 1502839586;
-	Meeter = 1510445899;
-	Noir = 1592917505;
-	Reeder = 6475002485;
-	Things3 = 904280696;
-	WindowsApp = 1295203466;
-	WireGuard = 1451685025;
+    Amphetamine = 937984704;
+    Dropover = 1355679052;
+    HandMirror = 1502839586;
+    Meeter = 1510445899;
+    Noir = 1592917505;
+    Reeder = 6475002485;
+    Things3 = 904280696;
+    WindowsApp = 1295203466;
+    WireGuard = 1451685025;
   };
 }
