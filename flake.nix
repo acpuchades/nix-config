cdf478e792ffb5871a20c75a0e956bad3204f860
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-11-03 01:08:10 +0100

    Pin flake versions to nixpkgs v25.05 (stable)

--- flake.nix
+++ flake.nix
@@ -2,34 +2,34 @@
   description = "Alejandro's nix-darwin system flake";
 
   inputs = {
-	# Nixpkgs
-	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Nixpkgs
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
 
-	# Nix-Darwin
-	nix-darwin.url = "github:nix-darwin/nix-darwin/master";
-	nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # Nix-Darwin
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-	# Home Manager
-	home-manager.url = "github:nix-community/home-manager";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-	# Sops-Nix
-	sops-nix.url = "github:Mic92/sops-nix";
-	sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    # Sops-Nix
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-	outputs@{
-	  self,
-	  nix-darwin,
-	  nixpkgs,
-	  home-manager,
-	  sops-nix
-	}:
-	{
-	  # Build darwin flake using:
-	  # $ sudo darwin-rebuild switch --flake .#MacBook-Pro-de-Alejandro
-	  darwinConfigurations."MacBook-Pro-de-Alejandro" = import ./hosts/macbookpro outputs;
-	  nixosConfigurations."homeserver" = import ./hosts/homeserver outputs;
-	};
+    outputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+      sops-nix
+    }:
+  {
+    # Build darwin flake using:
+    # $ sudo darwin-rebuild switch --flake .#MacBook-Pro-de-Alejandro
+    darwinConfigurations."MacBook-Pro-de-Alejandro" = import ./hosts/macbookpro outputs;
+    nixosConfigurations."homeserver" = import ./hosts/homeserver outputs;
+  };
 }
