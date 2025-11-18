1aaa0e02c42129a13a17ec7ababc1671b12385b2
acpuchades <59510094+acpuchades@users.noreply.github.com>
2025-11-18 04:28:27 +0100

    Configure mbsync with sops-nix and mu4e in emacs

---
+++ users/alex/programs/mbsync.nix
@@ -0,0 +1,26 @@
+{ config, ... }: {
+  enable = true;
+  extraConfig = ''
+    IMAPAccount icloud
+      Host imap.mail.me.com
+      User acaravacapuchades@icloud.com
+      PassCmd "cat ${config.sops.secrets."icloud/password".path}"
+      Port 993
+      TLSType IMAPS
+      AuthMechs LOGIN
+
+    IMAPStore icloud-remote
+      Account icloud
+
+    MaildirStore icloud-local
+      Path ~/Mail/
+      Inbox ~/Mail/INBOX
+
+    Channel icloud
+      Far :icloud-remote:
+      Near :icloud-local:
+      Patterns *
+      Create Both
+      SyncState *
+  '';
+}
