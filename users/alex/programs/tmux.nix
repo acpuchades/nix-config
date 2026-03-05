0d3a00f7a1f68ee4b1571c8129264a7405fb3293
acpuchades <59510094+acpuchades@users.noreply.github.com>
2026-03-05 23:54:53 +0100

    disable mouse support in tmux (it competes with emacs)

--- users/alex/programs/tmux.nix
+++ users/alex/programs/tmux.nix
@@ -3,12 +3,8 @@
   enable = true;
   shell = "${pkgs.zsh}/bin/zsh";
   terminal = "tmux-256color";
-  mouse = true;
   keyMode = "emacs";
   baseIndex = 1;
   escapeTime = 0;
   historyLimit = 1000000;
-  extraConfig = ''
-    set -as terminal-overrides ',*:XT'
-  '';
 }
