01bbcdeac8c86734ea9b7c8ca974a7a0514b8630
acpuchades <59510094+acpuchades@users.noreply.github.com>
2026-04-11 03:15:56 +0200

    include aider program settings

---
+++ users/alex/programs/aider-chat.nix
@@ -0,0 +1,8 @@
+{ ... }: {
+  enable = true;
+  settings = {
+    model = "claude-sonnet-4-20250514";
+    weak-model = "claude-haiku-4-5-20251001";
+    editor-model = "claude-opus-4-20250514";
+  };
+}
