b2f98783d8341f7c6f67e87932473d14bea3cd5f
acpuchades <59510094+acpuchades@users.noreply.github.com>
2026-04-11 04:37:08 +0200

    do not replace existing substituters with rstats-on-nix cache

--- modules/r-dev/system.nix
+++ modules/r-dev/system.nix
@@ -1,10 +1,12 @@
 { ... }:
+
 {
   nix.settings = {
-    substituters = [
+    extra-substituters = [
       "https://rstats-on-nix.cachix.org"
     ];
-    trusted-public-keys = [
+    extra-trusted-substituters = [ "https://rstats-on-nix.cachix.org" ];
+    extra-trusted-public-keys = [
       "rstats-on-nix.cachix.org-1:oMJOoBGKPOLGIKgRnMBbMXBTklu4SaBdd5SGGhIfOKQ="
     ];
   };
