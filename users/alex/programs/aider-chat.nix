c11c8a31a951181b5b57dae46aa6f4ecfaa19c8b
acpuchades <59510094+acpuchades@users.noreply.github.com>
2026-04-12 19:09:44 +0200

    replace aidermacs with claude-code

--- users/alex/programs/aider-chat.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  enable = true;
-  settings = {
-    model = "claude-sonnet-4-20250514";
-    weak-model = "claude-haiku-4-5-20251001";
-    editor-model = "claude-opus-4-20250514";
-  };
-}
--- users/alex/programs/zsh.nix
+++ users/alex/programs/zsh.nix
@@ -6,12 +6,6 @@
   syntaxHighlighting.enable = true;
   history.size = 10000;
   loginExtra = ''
-    # export api keys from secrets
-
-    if [[ -r '${config.sops.secrets."anthropic/token".path}' ]]; then
-      export ANTHROPIC_API_KEY="$(<'${config.sops.secrets."anthropic/token".path}')"
-    fi
-
     # mamba shell hook
     if command -v mamba >/dev/null 2>&1; then
       eval "$(${pkgs.mamba-cpp}/bin/mamba shell hook --shell zsh)"
@@ -40,14 +34,11 @@
       "z"
     ];
   };
-  plugins = [
-  {
+  plugins = [{
     name = "zsh-autosuggestions";
     src = pkgs.zsh-autosuggestions;
-  }
-  {
+  } {
     name = "zsh-syntax-highlighting";
     src = pkgs.zsh-syntax-highlighting;
-  }
-  ];
+  }];
 }
