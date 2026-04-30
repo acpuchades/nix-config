f89588deba305169c910cb803a256d316b494293
acpuchades <59510094+acpuchades@users.noreply.github.com>
2026-04-30 17:47:00 +0200

    use the right cachix public key in r-dev

--- modules/r-dev/system.nix
+++ modules/r-dev/system.nix
@@ -2,12 +2,9 @@
 
 {
   nix.settings = {
-    extra-substituters = [
-      "https://rstats-on-nix.cachix.org"
-    ];
-    extra-trusted-substituters = [ "https://rstats-on-nix.cachix.org" ];
+    extra-substituters = [ "https://rstats-on-nix.cachix.org" ];
     extra-trusted-public-keys = [
-      "rstats-on-nix.cachix.org-1:oMJOoBGKPOLGIKgRnMBbMXBTklu4SaBdd5SGGhIfOKQ="
+      "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
     ];
   };
 }
